21b4445cb3447aabba2b1877a5a2d30adf7b2445
Aasim-Qureshi <aasimq194@gmail.com>
2026-05-19 11:46:49 +0530

    pwd + edit

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+
 const connectDB = require("./config/db");
+
 const { router: itemsRouter } = require("./routes/items");
 const entriesRouter = require("./routes/entries");
-const items = require("./routes/items");
 const transactionsRouter = require("./routes/transactions");
-const authRouter = require("./routes/auth");
 
 connectDB();
 
 const app = express();
+
+app.set("trust proxy", 1);
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://inventory-frontend-sepia.vercel.app",
-    ],
+    origin: "https://inventory-frontend-sepia.vercel.app",
     credentials: true,
   }),
 );
-app.use(express.json());
 
-const session = require("express-session");
+app.use(express.json());
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "change-me-in-production",
     resave: false,
     saveUninitialized: false,
+
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      // required when sameSite is 'none'
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-
+      secure: true,
+      sameSite: "none",
       maxAge: 8 * 60 * 60 * 1000,
     },
   }),
@@ -46,6 +46,7 @@ app.use("/api/entries", entriesRouter);
 app.use("/api/transactions", transactionsRouter);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () =>
-  console.log(`Inventory API running on http://localhost:${PORT}`),
-);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
